4cadd86125002f7dd37238475ab51d9b3c09fab3
Saif <Saifsep17@gmail.com>
2024-05-12 11:58:34 +0530

    First Commit

---
+++ Frontend/rent_spot_frontend/src/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./reducers/rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Enable serializable check
+        ignoredActions: [], // Adjust as needed to allow certain non-serializable actions
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
